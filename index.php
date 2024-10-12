1ce6313811c1289b69f6f85a3a8cfb15392e41f0
carlosfuentes01 <carlos.fuentesp@cecar.edu.co>
2024-10-12 14:11:16 -0500

    explicacion de github

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      const data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+      ]);
+
+      const options = {
+        title: 'My Daily Activities',
+        pieHole: 0.4,
+      };
+
+      const chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+      chart.draw(data, options);
+    }
+  </script>
 </head>
+
 <body>
-    index de proyecto
+  index de proyect
+  <?php
+  $query =$conexion("select * from algo");
+  ?>
+  <div id="donutchart" style="width: 900px; height: 500px;"></div>
 </body>
+
 </html>
